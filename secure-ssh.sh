8c4387e5b83b126a2251a56d29271fa457b1ceaf
NEP-9842 <nicholasturner6@yahoo.com>
2019-02-27 23:51:26 -0500

    updeted script

--- secure-ssh.sh
+++ secure-ssh.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#SECURE-SSH.SH
+#AUTHOR NEP-9842
+#Creates a new ssh user
+#Adds a public key to that users authorized keys file
+#Removes roots ability to ssh in
+
 
 echo "Hello, Please enter you desired username: "
 
 read NEWUSER
-
 
 sudo useradd $NEWUSER
 
@@ -16,5 +21,7 @@ sudo chmod 700 /home/$NEWUSER/.ssh
 sudo chmod 600 /home/$NEWUSER/.ssh/authorized_keys
 
 sudo chown -R $NEWUSER:$NEWUSER /home/$NEWUSER/.ssh
+
+sudo sed -i -e 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
 echo "Your account has been created. NEP NEP!"
